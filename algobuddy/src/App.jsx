07a5bfe0153acf545cadeab05b76effe7d93deda
Teo Cristante <teo.cristante@gmail.com>
2025-04-11 16:15:38 -0400

    Last second doohickeys

--- algobuddy/src/App.jsx
+++ algobuddy/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   
   return (
     <div>
-      <VisualizeAlgorithm config={heapSortConfig} />
+      <VisualizeAlgorithm config={selectionSortConfig} />
     </div>
   );
 };
